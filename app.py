c0e20c594be945cac0133d4b903b11f0f9125bed
sampath Reddy <lsrsampath@gmail.com>
2026-01-07 21:14:18 +0530

    changed flask app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import socket
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    hostname = socket.gethostname()
+    return f"<h1>Served by pod: {hostname}</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
